518b13a287490e3a06540622edfeff95e5de50cf
huangjingyu2010 <huangjingyu2010@outlook.com>
2014-10-22 20:28:27 +0800

    test

--- src/cn/bupt/notes/MainActivity.java
+++ src/cn/bupt/notes/MainActivity.java
@@ -1,16 +1,30 @@
 package cn.bupt.notes;
 
 import android.R.integer;
+import android.app.Activity;
 import android.app.ListActivity;
+import android.content.Intent;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v4.widget.SimpleCursorAdapter;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ListView;
 import cn.bupt.notes.db.NotesDB;
 
 
 public class MainActivity extends ListActivity {
+	private OnClickListener btnAddNote_clickHandler = new OnClickListener() {
+		
+		@Override
+		public void onClick(View v) {
+			startActivityForResult(new Intent(MainActivity.this, AtyEditNote.class), REQUEST_CODE_ADD_NOTE);
+			
+		}
+	};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,14 +34,57 @@ public class MainActivity extends ListActivity {
         dbRead = db.getReadableDatabase();
         
 //        startActivityForResult(intent, requestCode)
-        adapter = new SimpleCursorAdapter(this, R.layout.notes_list_cell, null, new String[] {NotesDB.COLUMN_NAME_NOTE_NAME}, new int[]{R.id.tvDate});
+        adapter = new SimpleCursorAdapter(this, R.layout.notes_list_cell, null, new String[] {NotesDB.COLUMN_NAME_NOTE_NAME}, new int[]{R.id.tvName,R.id.tvDate});
         setListAdapter(adapter);
         refreshNotesListView();
+        
+        findViewById(R.id.btnAddNote).setOnClickListener(btnAddNote_clickHandler);
+        
+        
     }
+    
+    
     public void refreshNotesListView() {
     	adapter.changeCursor(dbRead.query(NotesDB.TABLE_NAME_NOTES, null, null, null, null, null, null));
-		
+//		刷新列表
 	}
+    
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+    	// TODO Auto-generated method stub
+    	Cursor c = adapter.getCursor();
+    	
+    	c.moveToPosition(position);
+    	
+    	Intent i = new Intent(MainActivity.this,AtyEditNote.class);
+    	i.putExtra(AtyEditNote.EXTRA_NOTE_ID, c.getColumnIndex(NotesDB.COLUMN_NAME_ID));
+    	i.putExtra(AtyEditNote.EXTRA_NOTE_NAME,c.getString(c.getColumnIndex(NotesDB.COLUMN_NAME_NOTE_NAME))) ;
+    	i.putExtra(AtyEditNote.EXTRA_NOTE_CONTENT,c.getString(c.getColumnIndex(NotesDB.COLUMN_NAME_NOTE_CONTENT))) ;
+    	startActivityForResult(i, REQUEST_CODE_EDIT_NOTE);
+    	super.onListItemClick(l, v, position, id);
+    }
+    
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+    	
+    	switch (requestCode) {
+    	
+		case REQUEST_CODE_ADD_NOTE:
+		case REQUEST_CODE_EDIT_NOTE:	
+			if (requestCode == Activity.RESULT_OK) {
+				refreshNotesListView();
+				
+				
+			}
+			
+			break;
+
+		default:
+			break;
+		}
+
+    	super.onActivityResult(requestCode, resultCode, data);
+    }
 
 
     @Override
@@ -53,5 +110,7 @@ public class MainActivity extends ListActivity {
     private NotesDB db;
     private SQLiteDatabase dbRead;
     private integer test;
+    public static final int REQUEST_CODE_ADD_NOTE = 1;
+    public static final int REQUEST_CODE_EDIT_NOTE = 1;
     
 }
